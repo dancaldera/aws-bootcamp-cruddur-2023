acfe0e961a3f88b62081349ede5af933bd548ebe
Daniel Caldera <bdcaldera@gmail.com>
2023-03-12 05:29:27 +0000

    db base shell files

---
+++ backend-flask/db/seed.sql
@@ -0,0 +1,21 @@
+-- this file was manually created
+INSERT INTO
+  public.users (display_name, handle, cognito_user_id)
+VALUES
+  ('Daniel Caldera', 'dancaldera', 'MOCK'),
+  ('Andrew Bayko', 'bayko', 'MOCK');
+INSERT INTO
+  public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (
+      SELECT
+        uuid
+      from
+        public.users
+      WHERE
+        users.handle = 'dancaldera'
+      LIMIT
+        1
+    ), 'This was imported as seed data!', current_timestamp + interval '10 day'
+  )
