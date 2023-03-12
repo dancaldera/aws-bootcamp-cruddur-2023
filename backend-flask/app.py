ce868f70a4f3eac3cee4cbebd8621c54810625c9
Daniel Caldera <bdcaldera@gmail.com>
2023-03-12 06:57:28 +0000

    home activities example fetching seeded data

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -161,20 +161,23 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 # xray_recorder.capture('activities_home')
 def data_home():
-    access_token = extract_access_token(request.headers)
-    try:
-        claims = cognito_jwt_token.verify(access_token)
-        # authenticated request
-        app.logger.debug("authenticated")
-        app.logger.debug(claims)
-        app.logger.debug(claims['username'])
-        data = HomeActivities.run(cognito_user_id=claims['username'])
-    except TokenVerifyError as e:
-        # unauthenticated request
-        app.logger.debug(e)
-        app.logger.debug("unauthenticated")
-        data = HomeActivities.run()
+    data = HomeActivities.run(cognito_user_id=None)
     return data, 200
+
+    # access_token = extract_access_token(request.headers)
+    # try:
+    #     claims = cognito_jwt_token.verify(access_token)
+    #     # authenticated request
+    #     app.logger.debug("authenticated")
+    #     app.logger.debug(claims)
+    #     app.logger.debug(claims['username'])
+    #     data = HomeActivities.run(cognito_user_id=claims['username'])
+    # except TokenVerifyError as e:
+    #     # unauthenticated request
+    #     app.logger.debug(e)
+    #     app.logger.debug("unauthenticated")
+    #     data = HomeActivities.run()
+    # return data, 200
 
 
 @app.route("/api/activities/notifications", methods=['GET'])
