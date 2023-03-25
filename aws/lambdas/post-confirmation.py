b5192d96e654b2047f5a6d95f602c02483065af0
Daniel Caldera <bdcaldera@gmail.com>
2023-03-25 09:25:52 +0000

    All Dynamo Completed

--- aws/lambdas/post-confirmation.py
+++ aws/lambdas/post-confirmation.py
@@ -1,32 +1,51 @@
 import json
-import psycopg2
-import os
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name='us-east-1',
+    endpoint_url="http://dynamodb.us-east-1.amazonaws.com"
+)
 
 
 def lambda_handler(event, context):
-    user = event['request']['userAttributes']
-    try:
-        print(user)
-        user_display_name = user['name']
-        user_email = user['email']
-        user_handle = user['preferred_username']
-        user_cognito_id = user['sub']
+    print('event-data', event)
 
-        sql = "INSERT INTO users (display_name, handle, email, cognito_user_id) VALUES (%s, %s, %s, %s)"
-        val = (user_display_name, user_handle, user_email, user_cognito_id)
+    eventName = event['Records'][0]['eventName']
+    if (eventName == 'REMOVE'):
+        print("skip REMOVE event")
+        return
+    pk = event['Records'][0]['dynamodb']['Keys']['pk']['S']
+    sk = event['Records'][0]['dynamodb']['Keys']['sk']['S']
+    if pk.startswith('MSG#'):
+        group_uuid = pk.replace("MSG#", "")
+        message = event['Records'][0]['dynamodb']['NewImage']['message']['S']
+        print("GRUP ===>", group_uuid, message)
 
-        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-        cur = conn.cursor()
-        cur.execute(sql, val)
-        conn.commit()
+        table_name = 'cruddur-messages'
+        index_name = 'message-group-sk-index'
+        table = dynamodb.Table(table_name)
+        data = table.query(
+            IndexName=index_name,
+            KeyConditionExpression=Key('message_group_uuid').eq(group_uuid)
+        )
+        print("RESP ===>", data['Items'])
 
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        # recreate the message group rows with new SK value
+        for i in data['Items']:
+            delete_item = table.delete_item(Key={'pk': i['pk'], 'sk': i['sk']})
+            print("DELETE ===>", delete_item)
 
-    finally:
-        if conn is not None:
-            cur.close()
-            conn.close()
-            print('Database connection closed.')
-
-    return event
+            response = table.put_item(
+                Item={
+                    'pk': i['pk'],
+                    'sk': sk,
+                    'message_group_uuid': i['message_group_uuid'],
+                    'message': message,
+                    'user_display_name': i['user_display_name'],
+                    'user_handle': i['user_handle'],
+                    'user_uuid': i['user_uuid']
+                }
+            )
+            print("CREATE ===>", response)
